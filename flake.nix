6548c51616abc7697a3c16db07a318366466626e
Multipixelone <finn@cnwr.net>
2025-04-22 23:16:58 -0400

    first commit

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Multipixelone playlist downloader";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pythonPackages = pkgs.python3Packages;
+      pythonModules = with pythonPackages; [
+        levenshtein
+        numpy
+      ];
+      python = pkgs.python3.override {
+        self = python;
+      };
+      app = pkgs.python3Packages.buildPythonApplication rec {
+        inherit python;
+        pname = "playlist-download";
+        version = "1.0";
+        format = "other";
+        src = ./.;
+
+        propagatedBuildInputs = pythonModules;
+
+        dontUnpack = true;
+        doCheck = false;
+        pytestCheckHook = false;
+
+        installPhase = ''
+          install -Dm755 ${src}/playlist-download.py $out/bin/${pname}
+          sed -i '1s|^|#!/usr/bin/env python3\n|' $out/bin/${pname}
+        '';
+        meta.mainProgram = "playlist-download";
+      };
+      env = pkgs.mkShell {
+        venvDir = "./.venv";
+        buildInputs = [
+          python
+          pythonModules
+          pythonPackages.venvShellHook
+          pkgs.autoPatchelfHook
+        ];
+        name = "playlist";
+        DIRENV_LOG_FORMAT = "";
+      };
+    in {
+      packages = {
+        playlist-download = app;
+        default = self.packages.${system}.playlist-download;
+      };
+      devShells.default = env;
+    });
+}
---
+++ playlist-download.py
@@ -0,0 +1,17 @@
+PLAYLIST_DIR = "/home/tunnel/Music/Playlists"
+MOPIDY_DIR = "/home/tunnel/.local/share/mopidy/m3u"
+IPOD_DIR = "/home/tunnel/Music/.ipod"
+MUSIC_DIR = "/media/Data/Music"
+# SECRET_FILE = "${config.age.secrets."plex".path}"
+PLAYLISTS = {
+    "monthly playlist": 24562,  # noqa
+    "forgotten faves": 48614,  # noqa
+    "good listening and learning": 20340,  # noqa
+    "slipped through": 26220,  # noqa
+    "vgm study": 53423,  # noqa
+    "amtrak": 26224,  # noqa
+    "mackin mabel": 61577,  # noqa
+    "summer jams": 61792,  # noqa
+}  # noqa
+
+print("test")
