2d1248c82b06defa476f68942b22e360e47a7056
Multipixelone <finn@cnwr.net>
2025-04-23 01:16:19 -0400

    fix names of playlists

--- playlist-download.py
+++ playlist-download.py
@@ -6,15 +6,13 @@ MOPIDY_DIR = "/home/tunnel/.local/share/mopidy/m3u"
 IPOD_DIR = "/home/tunnel/Music/.ipod"
 MUSIC_DIR = "/media/Data/Music"
 PLAYLISTS = {
-    "monthly playlist": 24562,  # noqa
-    "forgotten faves": 48614,  # noqa
-    "good listening and learning": 20340,  # noqa
-    "slipped through": 26220,  # noqa
-    "vgm study": 53423,  # noqa
-    "amtrak": 26224,  # noqa
-    "mackin mabel": 61577,  # noqa
-    "summer jams": 61792,  # noqa
-}  # noqa
+    "00 monthly playlist! :D": 24562,
+    "50 good listening & learning": 20340,
+    "02 vgm study": 53423,
+    "01 amtrak": 26224,
+    "y lastfm top all time": 0,
+    "y lastfm top six months": 1,
+}
 
 account = MyPlexAccount()
 plex = account.resource("alexandria").connect()
