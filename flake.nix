196c8f7d59ede9da09b8f6422ac4dd090f83636b
Multipixelone <finn@cnwr.net>
2025-10-20 00:16:28 -0400

    reformat: nix-fmt

--- flake.nix
+++ flake.nix
@@ -4,43 +4,52 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      # use git commit as version (i don't like this impl but i'll be brave)
-      version = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
-      pkgs = nixpkgs.legacyPackages.${system};
-      # python definitions & modules
-      pythonPackages = pkgs.python3Packages;
-      pythonModules = with pythonPackages; [
-        plexapi
-      ];
-
-      # packages
-      playlist-download = pkgs.callPackage ./playlist-download.nix {inherit pythonModules pythonPackages version;};
-      playlist-copy = pkgs.callPackage ./playlist-copy.nix {inherit pythonModules pythonPackages version;};
-
-      # devEnv
-      env = pkgs.mkShell {
-        venvDir = "./.venv";
-        buildInputs = [
-          pythonModules
-          pythonPackages.python
-          pythonPackages.venvShellHook
-          pkgs.autoPatchelfHook
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # use git commit as version (i don't like this impl but i'll be brave)
+        version = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
+        pkgs = nixpkgs.legacyPackages.${system};
+        # python definitions & modules
+        pythonPackages = pkgs.python3Packages;
+        pythonModules = with pythonPackages; [
+          plexapi
         ];
-        name = "playlist";
-        DIRENV_LOG_FORMAT = "";
-      };
-    in {
-      packages = {
-        playlist-download = playlist-download;
-        playlist-copy = playlist-copy;
-        default = self.packages.${system}.playlist-download;
-      };
-      devShells.default = env;
-    });
+
+        # packages
+        playlist-download = pkgs.callPackage ./playlist-download.nix {
+          inherit pythonModules pythonPackages version;
+        };
+        playlist-copy = pkgs.callPackage ./playlist-copy.nix {
+          inherit pythonModules pythonPackages version;
+        };
+
+        # devEnv
+        env = pkgs.mkShell {
+          venvDir = "./.venv";
+          buildInputs = [
+            pythonModules
+            pythonPackages.python
+            pythonPackages.venvShellHook
+            pkgs.autoPatchelfHook
+          ];
+          name = "playlist";
+          DIRENV_LOG_FORMAT = "";
+        };
+      in
+      {
+        packages = {
+          playlist-download = playlist-download;
+          playlist-copy = playlist-copy;
+          default = self.packages.${system}.playlist-download;
+        };
+        devShells.default = env;
+      }
+    );
 }
